d14d1b7da584b78e1d530196d0cf21a91d4c8185
Thad <73568235+mmmmm-art@users.noreply.github.com>
2022-11-09 15:41:40 -0700

    sqaure

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,4 +1,17 @@
-let canvas = document.getElementById("CANVA_1")
+let canvas = document.getElementById("game-canvas")
+/** @type {CanvasRenderingContext2D} */
 let ctx = canvas.getContext("2d");
+canvas.width = 1400
+canvas.height = 800
 
-ctx.fillRect(200,200,100,100)
+let squares = [];
+// s.addPath(ctx.fillStyle = `hsla(${c},100%,50%,1)`)
+function drawSquare(x,y,s,c) {
+	let square = new Path2D();
+	square.rect(x,y,s,s)
+	squares.push(square);
+}
+drawSquare(0,0,100,100,0)
+
+console.log(squares)
+
